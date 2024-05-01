08329ecb47800f447e2a939ad0eb5d2cff727506
me990928 <me990928@gmail.com>
2024-05-02 04:51:52 +0900

    計算メソッドの修正

--- app/src/test/java/com/example/hachisalarycalc/SalaryCalculator.kt
+++ app/src/test/java/com/example/hachisalarycalc/SalaryCalculator.kt
@@ -12,27 +12,27 @@ class SalaryCalculator(
     private val doubleMultiplier: Double
 ) {
     // 手当なしの日給
-    private fun calcNormalSalary(): Int {
+    fun calcNormalSalary(): Int {
         val salary = workTime * wage
         return ceil(salary).toInt()
     }
     // 残業手当のみ
-    private fun calcOverWorkSalary(): Int {
+    fun calcOverWorkSalary(): Int {
         val salary = (normalWork * wage.toDouble()) + ((workTime - normalWork) * wage.toDouble() * singleMultiplier)
         return ceil(salary).toInt()
     }
     // 夜勤手当のみ
-    private fun calcNightSalary(): Int {
+    fun calcNightSalary(): Int {
         val salary = (dayWorkTime * wage.toDouble()) + (nightWorkTime * wage.toDouble() * singleMultiplier)
         return ceil(salary).toInt()
     }
     // 残業して夜勤
-    private fun calcOverAfterNightSalary(): Int {
+    fun calcOverAfterNightSalary(): Int {
         val salary = (normalWork * wage.toDouble()) + ((dayWorkTime - normalWork) * wage.toDouble() * singleMultiplier) + (nightWorkTime * wage.toDouble() * doubleMultiplier)
         return ceil(salary).toInt()
     }
     // 夜勤して残業
-    private fun calcNightAfterOverSalary(): Int {
+    fun calcNightAfterOverSalary(): Int {
         val salary = (dayWorkTime * wage.toDouble()) + ((normalWork - dayWorkTime) * wage.toDouble() * singleMultiplier) + ((workTime - normalWork) * wage.toDouble() * doubleMultiplier)
         return ceil(salary).toInt()
     }
